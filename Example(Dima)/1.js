08b62f5ef6f248f9e3a6ff9961b185cd249ed35f
bookgvi <bookgvi@gmail.com>
2019-08-19 19:25:58 +0300

    open/close modal Windows

--- Example(Dima)/1.js
+++ Example(Dima)/1.js
@@ -1,55 +1,27 @@
-    /*modal*/
-    //    let frstModal = document.querySelector('.frst-popup')
-    //    let Modal1 = new Modal(frstModal)
-    //    .onclick = Modal1.modalOpen()
-    
-    // function Modal(open){
-    
-    //     this.open = open;
-    //     this.close = document.querySelector('.popup-head span');
-    //     let that = this;
-    
-    //     this.modalOpen = function(){
-    //             that.open.style.display = "block"
-    //     };
-    
-    //     this.modalClose = function() {
-    //         that.open.style.display = "none"
-    //     };
-    
-    //     // this.close.onclick = that.modalclose();
-    
-    // }
-    function Modal(modal) {
-            this.modal = document.querySelector('.frst-popup');
-            this.btn1 = document.querySelector('.button-frst');
-            this.btnOfpopup = document.querySelector('form .button-frst')
-            self = this;
-
-        this.popup = function(e) {                          // вот здесь нужно проверять на какую кнопку или элемент нажали 
-                if (e.target == self.btn1)                  // (можно вместо if...else использовать switch...case)
-                    self.modal.style.display = 'block'
-                else if (e.target == self.btnOfpopup)
-                    self.modal.style.display = 'none'
-            }
+function Modal() {
+  this.m = new Map();
+  this.b = document.querySelectorAll('[data-popup]')
+  this.b.forEach((item) => {
+    if(item.tagName=='A') {
+      this.m.set(item.dataset.popup, document.querySelector(`[data-modal~="${item.dataset.popup}"]`))
     }
+  })
+  // this.modal = document.querySelector('.popup');
+  // this.btn1 = document.querySelector('.button-frst');
+  // this.btnOfpopup = document.querySelector('form .button-frst')
+  self = this
 
-//    new Modal()
+  this.popup = function(e) {
+    if (self.m.has(e.target.dataset.popup)) {
+      self.m.get(e.target.dataset.popup).style.display = 'block'
+    }
+    else if (e.target.dataset.modal) {
+      const modal = document.querySelector(`[data-modal="${e.target.dataset.modal}"]`)
+      modal.style.display = 'none'
+    }
+  }
+  document.body.onclick = this.popup;
 
-class ModalClass {
-    constructor() {
-        document.body.onclick = this.popup;
-    }
-    popup(e) {
-        this.modal = document.querySelector('.frst-popup');
-        this.btn1 = document.querySelector('.button-frst');
-        this.btnOfpopup = document.querySelector('form .button-frst')
-        if (e.target == this.btn1)                  
-            this.modal.style.display = 'block'
-        else if (e.target == this.btnOfpopup)
-            this.modal.style.display = 'none'
-    }
 }
-
-new ModalClass();
+new Modal();
 
---
+++ filter.js
@@ -0,0 +1,6 @@
+arr = [1,2,3,4,5]
+inBetween = function(x){
+  return x > 3
+}
+
+console.log(arr.filter(inBetween))

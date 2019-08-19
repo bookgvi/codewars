c9493e29935da92d06307b187b0da582764fcbe6
bookgvi <bookgvi@gmail.com>
2019-08-19 22:18:53 +0300

    Masha pila sok)) Возведение в степень длин слов в массиве с обратном порядком, сделал в 1 строку

--- Masha_pila_sok.js
+++ Masha_pila_sok.js
@@ -1,3 +1,8 @@
-arr = []
-'Masha pila sok'.split(' ').forEach(i => {arr.push(i.length)})
-console.log(arr.reverse().reduce((prev, cur) =>  Math.pow(prev, cur)))
+console.log('Masha pila sok'.split(' ').map(i=>i.length).reverse().reduce((acc, cur) => Math.pow(acc, cur)))
+
+// -----------------------------------------------------------------
+inBeetween = (a,b) => {
+  return x => a<= x & x <= b
+}
+console.log([1,2,3,4,5,6,7,8,9].filter(inBeetween(3,7)))
+// -----------------------------------------------------------------

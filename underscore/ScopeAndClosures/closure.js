52879537c510a77d32bbb424ee8bf5470b1c3f61
bookgvi <bookgvi@gmail.com>
2019-08-20 17:09:13 +0300

    Functional JS scope and closures + higher-level functions

---
+++ underscore/ScopeAndClosures/closure.js
@@ -0,0 +1,24 @@
+function initMap(val){
+  return function closure(arr){
+    this.val = val;
+    self = this
+    return arr.map((function(item){
+      return item * this.val
+    }).bind(self))
+  }
+}
+
+start = initMap(10);
+console.log(start.call(null, [1,2,3]))
+
+
+function average(arr){
+  return (arr.reduce((acc, cur) => acc+cur))/arr.length
+}
+function averageInit(value1){
+  return (value2)=> {
+    return average([value2, value1(value2)])
+  }
+}
+avResult = averageInit(n=>n*n)
+console.log(avResult(4))

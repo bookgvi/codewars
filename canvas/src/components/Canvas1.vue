b29e4743e84b1a5d42b1c9290f9f6e46ba0e8323
bookgvi <bookgvi@gmail.com>
2019-08-24 03:12:25 +0300

    Canvas: Learning paining. Moveable padding

--- canvas/src/components/Canvas1.vue
+++ canvas/src/components/Canvas1.vue
@@ -11,38 +11,87 @@
 <script>
 export default {
   data: () => ({
-    intId: ''
+    intId: '',
+    rightPress: false,
+    leftPress: false
+
   }),
   methods: {
     stopMovement() {
       clearInterval(this.intId)
+      this.rightPress = false
+      this.leftPress = false
     },
     drawBtn() {
       let canvas, ctx
       canvas = document.getElementById('canvas')
       ctx = canvas.getContext('2d')
-      let x = 20, y = 20, dx = 2, dy = 2
+
+      document.addEventListener('keyup', this.hKeyUp)
+      document.addEventListener('keydown', this.hKeyDown)
+
+      let ballX = 20, ballY = 20, dx = 2, dy = 2
       let radius = 10
+      let paddleHeight = 10, paddleWidth = 75
+      let paddleX = (canvas.width - paddleWidth) / 2
 
       this.intId = setInterval(()=>{
-        this.ball(canvas, ctx, x, y, radius)
-        x += dx
-        y += dy
-        if(y >= canvas.height - radius || y <= 0 + radius) {
+        this.ball(canvas, ctx, ballX, ballY, radius)
+        this.paddle(canvas, ctx, paddleX, paddleWidth, paddleHeight)
+        ballX += dx
+        ballY += dy
+        if(ballY >= canvas.height - radius || ballY <= 0 + radius) {
           dy = -dy
         }
-        if(x >= canvas.width - radius || x <= 0 + radius) {
+        if(ballX >= canvas.width - radius || ballX <= 0 + radius) {
           dx = -dx
         }
+
+        if(this.leftPress) {
+          paddleX -= 7
+          if (paddleX < 0) {
+            paddleX = 0
+          }
+        }
+        else if (this.rightPress) {
+          paddleX += 7
+          if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth
+          }
+        }
+
       },10)
     },
-    ball(canvas, ctx, x, y, radius) {
+    ball (canvas, ctx, x, y, radius) {
       ctx.clearRect(0, 0, canvas.width, canvas.height)
       ctx.beginPath()
       ctx.arc(x, y, radius, 0, Math.PI*2)
       ctx.fillStyle = '#0095dd'
       ctx.fill()
       ctx.closePath()
+    },
+    paddle (canvas, ctx, paddleX, pWidth, pHeight) {
+      ctx.beginPath()
+      ctx.rect(paddleX, canvas.height - pHeight, pWidth, pHeight)
+      ctx.fillStyle = '#0095dd'
+      ctx.fill()
+      ctx.closePath()
+    },
+    hKeyUp (e) {
+      if (e.key == 'ArrowLeft' || e.key == 'Left' || e.code == 'KeyA') {
+        this.leftPress = false
+      }
+      if (e.key == 'ArrowRight' || e.key == 'Right' || e.code == 'KeyD') {
+        this.rightPress = false
+      }
+    },
+    hKeyDown (e) {
+      if(e.key == 'ArrowLeft' || e.key == 'Left' || e.code == 'KeyA') {
+        this.leftPress = true
+      }
+      if(e.key == 'ArrowRight' || e.key == 'Right' || e.code == 'KeyD') {
+        this.rightPress = true
+      }
     }
   }
 }

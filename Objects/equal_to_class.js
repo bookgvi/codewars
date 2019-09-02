d1a6c5424c7a702da1f124d591a13b45d03d92e9
bookgvi <bookgvi@gmail.com>
2019-09-02 17:18:10 +0300

    Object Class

--- Objects/equal_to_class.js
+++ Objects/equal_to_class.js
@@ -4,13 +4,13 @@ User = (function() {
   function user2 (a) {
       this.a = a
   }
-  Object.defineProperty(user2.prototype, 'b', {
+  Object.defineProperty(user2.prototype, 'a', {
     get: function () {
-      return this.a
+      return 'aaaaaaaa'
     }
   })
 return user2;
 })()
 
 obj1 = new User('aaa')
-console.log(obj1.b)
+console.log(obj1.a)

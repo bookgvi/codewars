ebc96ef0ae8a4de73bcaa40b17f7b22bd69b5f37
bookgvi <bookgvi@gmail.com>
2019-09-02 19:15:41 +0300

    Class is equal the same

--- Objects/equal_to_class2.js
+++ Objects/equal_to_class2.js
@@ -2,11 +2,12 @@ User = (() => {
   function userIn (a) {
     this.a = a
   }
-  Object.defineProperty(userIn.prototype, 'name', {
-    get: function () { this.a }
+  Object.defineProperty(userIn.prototype, 'aa', {
+    get: function () { return this.a }
   })
   return userIn
 })()
 
 obj1 = new User('qq')
-console.log(obj1.name)
+console.log(obj1.a)
+console.log(obj1.aa)

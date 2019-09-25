b6c495290dd53d11fe7b930a28586ebb7fcc7d6a
book <bookgvi@gmail.com>
2019-09-25 21:35:07 +0300

    recursive

--- Grokaem algoritmy/quickSort.js
+++ Grokaem algoritmy/quickSort.js
@@ -1,12 +1,11 @@
 function summ(arr, i, sum) {
-  if (i > arr.length) {
+  if (i >= arr.length) {
     return sum
   }
-  console.log(sum)
   sum += arr[i]
-  summ(arr, i + 1, sum)
+  return summ(arr, i + 1, sum)
 }
 
 arr = [1, 2, 3]
-const sum = summ(arr, 0, 0)
-console.log(sum)
+let sum = summ(arr, 0, 0)
+console.log(summ(arr, 0, 0))

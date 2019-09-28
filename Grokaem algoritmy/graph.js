6c8f888095e1581c2404ca909c75b5d1c4c447bd
book <bookgvi@gmail.com>
2019-09-28 12:28:13 +0300

    graph

--- Grokaem algoritmy/graph.js
+++ Grokaem algoritmy/graph.js
@@ -1,3 +1,9 @@
 const start = new Map()
-const start = new Map()
+const a = new Map()
 const b = new Map()
+
+start.set('a', 6)
+start.set('b', 2)
+a.set('finish', 1)
+b.set('a', 3)
+b.set('finish', 5)

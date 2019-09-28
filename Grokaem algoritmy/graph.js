ff5d735fc1dabde314f76efc27456d88b5f5e012
book <bookgvi@gmail.com>
2019-09-28 21:38:19 +0300

    graph

--- Grokaem algoritmy/graph.js
+++ Grokaem algoritmy/graph.js
@@ -1,9 +1,25 @@
-const start = new Map()
+const s = new Map()
 const a = new Map()
 const b = new Map()
+const f = new Map()
+const graph = [s, b, a, f]
+s.set(b, 2)
+s.set(a, 6)
+a.set(f, 1)
+b.set(a, 3)
+b.set(f, 5)
 
-start.set('a', 6)
-start.set('b', 2)
-a.set('finish', 1)
-b.set('a', 3)
-b.set('finish', 5)
+const sa = s.get(a)
+const sb = s.get(b)
+const ba = b.get(a)
+const bf = b.get(f)
+const af = a.get(f)
+
+// graph.forEach((item, index) => item.forEach((item, index) => index.forEach((item, index) => console.log(index, item)), item))
+
+grpahWeight = graph => {
+  // if(!graph) { return }
+  graph.forEach((item, key) => grpahWeight(key))
+}
+
+console.log(grpahWeight(s))

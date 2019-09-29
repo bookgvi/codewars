471426313d56554ebaad167f3867e97d3ddc9044
book <bookgvi@gmail.com>
2019-09-29 10:42:31 +0300

    Вариант графа через объекты

--- Grokaem algoritmy/graph.js
+++ Grokaem algoritmy/graph.js
@@ -1,25 +1,46 @@
-const s = new Map()
-const a = new Map()
-const b = new Map()
-const f = new Map()
-const graph = [s, b, a, f]
-s.set(b, 2)
-s.set(a, 6)
-a.set(f, 1)
-b.set(a, 3)
-b.set(f, 5)
-
-const sa = s.get(a)
-const sb = s.get(b)
-const ba = b.get(a)
-const bf = b.get(f)
-const af = a.get(f)
-
-// graph.forEach((item, index) => item.forEach((item, index) => index.forEach((item, index) => console.log(index, item)), item))
-
-grpahWeight = graph => {
-  // if(!graph) { return }
-  graph.forEach((item, key) => grpahWeight(key))
+// const s = new Map()
+// const a = new Map()
+// const b = new Map()
+// const f = new Map()
+// const graph = [s, b, a, f]
+// s.set(b, 2)
+// s.set(a, 6)
+// a.set(f, 1)
+// b.set(a, 3)
+// b.set(f, 5)
+//
+// const sa = s.get(a)
+// const sb = s.get(b)
+// const ba = b.get(a)
+// const bf = b.get(f)
+// const af = a.get(f)
+//
+// grpahWeight = graph => {
+//   if(!graph) { return graph}
+//   for (key in graph) {
+//     return grpahWeight(key)
+//   }
+// }
+//
+const s = {
+  a: 6,
+  b: 2
+}
+const a = {
+  f: 1
+}
+const b = {
+  a: 3,
+  f: 5
 }
 
-console.log(grpahWeight(s))
+function graph (gr) {
+  for (key in gr) {
+    if (!key) {
+      return graph(key)
+    }
+    console.log(gr[key], graph(key))
+  }
+}
+
+console.log(graph(s))

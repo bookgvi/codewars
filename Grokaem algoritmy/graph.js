1e11f4eaa410152dbcb969f3fa9b8e0f94d4a6c9
book <bookgvi@gmail.com>
2019-10-01 00:00:19 +0300

    вывод узлов графа

--- Grokaem algoritmy/graph.js
+++ Grokaem algoritmy/graph.js
@@ -1,40 +1,17 @@
-// const s = new Map()
-// const a = new Map()
-// const b = new Map()
-// const f = new Map()
-// const graph = [s, b, a, f]
-// s.set(b, 2)
-// s.set(a, 6)
-// a.set(f, 1)
-// b.set(a, 3)
-// b.set(f, 5)
-//
-// const sa = s.get(a)
-// const sb = s.get(b)
-// const ba = b.get(a)
-// const bf = b.get(f)
-// const af = a.get(f)
-//
-// data = {
-//   s: {
-//     a: {
-//       b: {
-//         f: {}
-//       }
-//     },
-//     b: {
-//       f: {}
-//     }
-// }
-//   }
-const f = {}
+const f = 1
 const b = { 5: f }
 const a = { 1: f, 3: b }
 
 const s = { 6: a, 2: b }
 
-for(let key in s) {
-  console.log(key, s[key])
+function showWeight (obj) {
+  Object.keys(obj).forEach(item => {
+    if (typeof obj[item] === 'object') {
+      console.log('o', item)
+      showWeight(obj[item])
+    } else {
+      console.log('i', item)
+    }
+  })
 }
-// console.log(data)
-// graph.forEach((item, index) => index.forEach(item1 => console.log(item, index)))
+showWeight(s)

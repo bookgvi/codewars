e5d1180a6dba739f619687923565cfa241756a0d
book <bookgvi@gmail.com>
2019-10-02 21:22:11 +0300

    модули

---
+++ modules/script.js
@@ -0,0 +1,18 @@
+import { sayHi } from './main.js'
+
+sayHi('Dima')
+
+let entiries = {
+  name: 'Dima',
+  age: 21,
+  job: verstka
+}
+
+let obj = [
+  ['name', 'Dima'],
+  ['age', 21],
+  ['job', 'verstka']
+]
+
+console.log(obj)
+console.log(entries)

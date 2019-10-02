7ce057327a48a0c965d5c4e558b04f0e075e75b0
book <bookgvi@gmail.com>
2019-10-03 00:32:29 +0300

    поменять местами значения переменных

--- Grokaem algoritmy/exchange2counts.js
+++ Grokaem algoritmy/exchange2counts.js
@@ -1,4 +1,4 @@
-let a = 1, b = 2
+let a = 500, b = 20
 //
 // // 1)
 let c = a
@@ -11,12 +11,13 @@ a = a + b
 b = a - b
 a = a - b
 console.log(a, b)
-//
-// // 3) в node.js работать не будет!!!!!
-// // [b, a] = [a, b]
-// // console.log(a, b)
-//
-// // 4)
+
+// 3)
+fn = _ => { [b, a] = [a, b] }
+fn()
+console.log(a, b)
+
+// 4)
 arr = [a, b].reverse()
 b = arr.pop()
 a = arr.pop()
@@ -27,4 +28,9 @@ console.log(a, b)
 a = a ^ b
 b = b ^ a
 a = a ^ b
+console.log(a, b)
+
+// 6)
+fn = _ => { [a, b] = [a, b].reduce((curr, next) => { return [next, curr] }) }
+fn()
 console.log(a, b)

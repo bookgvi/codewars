40a61a7da8a3943783a404d3aea487bdf72c74f8
book <bookgvi@gmail.com>
2019-10-06 13:49:04 +0300

    NeighborsCount: released for 2d arrays

---
+++ Grokaem algoritmy/neighborsInArray.js
@@ -0,0 +1,52 @@
+const arr = []
+
+function create2dArray(arr, size) {
+  let row = 0
+  let col = 0
+  const length = size
+  while (row <= length) {
+    arr[row] = []
+    while(col <= length) {
+      arr[row][col] = 0
+      col++
+    }
+    row++
+    col = 0
+  }
+  return arr
+}
+
+function neighborsCount(arr) {
+  let neighborsCount = 0
+  let neighbors = []
+  let row = 0
+  let col = 0
+  while (row < arr.length) {
+    neighbors[row] = []
+    while (col < arr[row].length) {
+      neighborsCount = 0
+      if (arr[row][col - 1] === 0 || arr[row][col - 1] ) { neighborsCount++ }
+      if (arr[row][col + 1] === 0 || arr[row][col + 1] ) { neighborsCount++ }
+      let item = row - 1
+      if (arr[item]) {
+        if (arr[item][col - 1] === 0 || arr[item][col - 1]) { neighborsCount++ }
+        if (arr[item][col] === 0 || arr[item][col]) { neighborsCount++ }
+        if (arr[item][col + 1] === 0 || arr[item][col + 1]) { neighborsCount++ }
+      }
+      item = row + 1
+      if (arr[item]) {
+        if (arr[item][col - 1] === 0 || arr[item][col - 1]) { neighborsCount++ }
+        if (arr[item][col] === 0 || arr[item][col]) { neighborsCount++ }
+        if (arr[item][col + 1] === 0 || arr[item][col + 1]) { neighborsCount++ }
+      }
+      neighbors[row][col] = neighborsCount
+      col++
+    }
+    row++
+    col = 0
+  }
+  return neighbors
+}
+
+console.log('Create empty array: \n', create2dArray(arr, 5))
+console.log('Neighbors: \n', neighborsCount(arr))

aa8e4ed7c83060ab6ffc32c1de8c4a8b7097501c
book <bookgvi@gmail.com>
2019-10-13 14:02:45 +0300

    Sort algos: change array length

--- Grokaem algoritmy/sortAlgos.js
+++ Grokaem algoritmy/sortAlgos.js
@@ -54,7 +54,7 @@ function selectionSort (arr) {
   return arr
 }
 
-const length = 100000
+const length = 25000
 function execTime(sortAlgo, label) {
   let arr = []
   for (let i = 1; i < length; i++) {

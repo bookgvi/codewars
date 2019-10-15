8823b67b330b3d84de2d44e09f7fd95e1b713c9e
book <bookgvi@gmail.com>
2019-10-15 21:49:58 +0300

    XHR

--- XHR/xhr.js
+++ XHR/xhr.js
@@ -2,7 +2,7 @@ const url = new URL('https://jsonplaceholder.typicode.com/posts/1')
 const xhr = new XMLHttpRequest()
 xhr.open('GET', url)
 xhr.send()
-xhr.load(() => {
+xhr.onload = () => {
   if (xhr.status !== 200) {
     return Error
   } else {

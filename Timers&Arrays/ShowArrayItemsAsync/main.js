5517f008b043bba71da08a5a5bbf859acbae1f2a
bookgvi <bookgvi@gmail.com>
2020-03-01 13:54:02 +0300

    Реализация утилит через литеральную нотацию объекта

--- Timers&Arrays/ShowArrayItemsAsync/main.js
+++ Timers&Arrays/ShowArrayItemsAsync/main.js
@@ -1,4 +1,5 @@
 import { iterate } from './iterate.js';
-import { utils } from './utils.js';
+import { utils, UtilsLiteral } from './utils.js';
 
 iterate(utils.arr, utils.handler.bind(utils), utils.cb.bind(utils));
+iterate(UtilsLiteral.arr, UtilsLiteral.handler.bind(UtilsLiteral), UtilsLiteral.cb.bind(UtilsLiteral));
--- Timers&Arrays/ShowArrayItemsAsync/utils.js
+++ Timers&Arrays/ShowArrayItemsAsync/utils.js
@@ -22,3 +22,24 @@ Utils.prototype.cb = function () {
 }
 
 export const utils = new Utils();
+
+/**
+ * Те же утилиты, только в другой нотации
+ */
+export const UtilsLiteral = {
+  SIZE: 1000,
+  fragment: document.createDocumentFragment(),
+
+  get arr () {
+    return Array.apply(UtilsLiteral, { length: this.SIZE }).map(() => (Math.random() * this.SIZE + 1).toFixed(0))
+  },
+
+  handler (item) {
+    const span = document.createElement('span');
+    span.textContent = `${item} `;
+    this.fragment.appendChild(span);
+  },
+  cb () {
+    document.body.appendChild(this.fragment);
+  }
+};
